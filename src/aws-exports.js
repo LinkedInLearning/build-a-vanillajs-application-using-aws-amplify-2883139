4d2a897268881712399a5eeb38ddb1df0678f3c8
Ali Spittel <ali07cat07@gmail.com>
2021-05-17 16:07:43 -0500

    complete code for lesson 02_05

---
+++ src/aws-exports.js
@@ -0,0 +1,5 @@
+const awsmobile =  {
+}
+
+
+export default awsmobile
--- src/script.js
+++ src/script.js
@@ -1,6 +1,33 @@
-import Amplify from 'aws-amplify'
+import Amplify, { DataStore } from 'aws-amplify'
+import { Post } from './models'
+
 import awsconfig from './aws-exports'
 
 Amplify.configure(awsconfig)
 
-console.log('hi');
+const pullData = async () => {
+  try {
+    const posts = await DataStore.query(Post)
+    console.log(posts)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+pullData()
+
+document.getElementById('create-post').addEventListener('click', async e => {
+  e.preventDefault()
+  try {
+    const newPost = await DataStore.save(
+      new Post({
+        link: 'https://binaryville.com/images/characters/dolores-disc.png',
+        description: "Felt cute might delete",
+        postedTime: new Date().toISOString()
+      })
+    )
+    console.log(newPost)
+  } catch (error) {
+    console.error(error)
+  } 
+})
