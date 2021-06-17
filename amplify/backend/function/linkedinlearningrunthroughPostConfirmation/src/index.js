9fde34c37b1072111b25b760fe5ae5db3416d60b
Ali Spittel <ali07cat07@gmail.com>
2021-06-17 11:38:38 -0500

    05_02

--- amplify/backend/function/linkedinlearningrunthroughPostConfirmation/src/index.js
+++ amplify/backend/function/linkedinlearningrunthroughPostConfirmation/src/index.js
@@ -9,34 +9,6 @@ Amplify Params - DO NOT EDIT *//*
   provided that the file names (without extension) are included in the "MODULES" env variable.
   "MODULES" is a comma-delimmited string.
 */
-const aws = require('aws-sdk')
-const ddb = new aws.DynamoDB()
-
 exports.handler = async (event, context) => {
-  const date = new Date()
-  const params = {
-    Item: {
-      __typename: { S: 'Author' },
-      id: { S: event.request.userAttributes.sub },
-      name: { S: event.userName },
-      profilePic: {
-        S:
-        'https://placekitten.com/640/360'
-      },
-      createdAt: { S: date.toISOString() },
-      updatedAt: { S: date.toISOString() },
-      _version: { N: '1' },
-      _lastChangedAt: { N: `${date.getTime()}` }
-    },
-    TableName: process.env.API_LINKEDINLEARNINGRUNTHROUGH_AUTHORTABLE_NAME
-  }
-
-  try {
-    const item = await ddb.putItem(params).promise()
-    console.log(item)
-  } catch (err) {
-    console.error(err)
-  }
-
   context.done(null, event)
 }
